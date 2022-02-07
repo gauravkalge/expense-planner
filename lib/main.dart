31cd199f011d5dbdcf054b011672f5bd8ed5ffc9
Gaurav Kalge <42073937+kalge@users.noreply.github.com>
2022-02-07 10:10:05 +0530

    error

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expense_planner/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -15,17 +16,45 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+final List<Transaction> transaction = [
+    Transaction(
+    id:'t1',
+    title:'new shoes',
+    amount: 10.77,
+    date: DateTime.now(),
+      ),
 
+    Transaction(
+    id:'t2',
+    title:'bag',
+    amount: 80.78,
+    date: DateTime.now(),
+      )
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter App'),
-      ),
-      body: const Center(
-        child: Text('Widget Playground!'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Flutter App'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+                width: double.infinity,
+                child: const Card(
+                  color: Colors.blueAccent,
+                  child: Text('Chart'),
+                  elevation: 5,
+                )),
+            const Card(
+              color: Colors.red,
+              child: Text('transaction'),
+            ),
+            const Card(
+              child: Text('gjghhvgkhh'),
+            )
+          ],
+        ));
   }
 }
